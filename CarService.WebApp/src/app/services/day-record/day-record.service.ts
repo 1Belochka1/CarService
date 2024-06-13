737196466bba8e56a77a06b9ccdf0f844f2e1d29
belochka <hologtbel@gmail.com>
2024-06-14 00:53:30 +0300

    запись на время успешно работает

--- CarService.WebApp/src/app/services/day-record/day-record.service.ts
+++ CarService.WebApp/src/app/services/day-record/day-record.service.ts
@@ -44,6 +44,7 @@ export class DayRecordService implements OnDestroy {
 	}
 
 	updateRecord(id: string, email: string, phone: string, name: string) {
+		console.log(id, email, phone, name)
 		this._hubConnection.invoke('RecordTimeRecord', id, true, email, phone, name)
 				.then(() => {
 					this._toastr.success('Вы записаны')
--- CarService.WebApp/src/environments/environment.development.ts
+++ CarService.WebApp/src/environments/environment.development.ts
@@ -2,5 +2,4 @@ export const environment = {
 	api: 'http://localhost:5053/api/',
 	// api: 'http://localhost:8080/api/',
 	// api: 'http://45.8.99.153/api/',
-
 }
